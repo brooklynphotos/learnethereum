bef2259ed25ca5ea1dc3a3150cb710d7c588f977
Brooklyn Photos <guoneng.list@gmail.com>
2019-05-07 22:22:54 +0200

    using struct to help vote using the name instead of the index

--- Voter.sol
+++ Voter.sol
@@ -5,16 +5,28 @@ contract Voter {
   uint[] public votes;
   string[] public options;
   mapping(address=>bool) hasVoted;
+  mapping(string=>OptionPos) posOfOption;
+
+  struct OptionPos{
+    uint pos;
+    bool exists;
+  }
 
   constructor(string[] memory _options) public {
     options = _options;
     votes.length = options.length;
+    for (uint i = 0; i < options.length; i++) {
+      // using memory to avoid a cost
+      OptionPos memory optPos = OptionPos(i, true);
+      posOfOption[options[i]] = optPos;
+    }
   }
 
-  function vote(uint option) public {
-    require(option >= 0 && option<options.length, "Invalid option");
+  function vote(string memory option) public {
     require(!hasVoted[msg.sender], "Account already voted");
-    votes[option] = votes[option] + 1;
+    OptionPos memory opPos = posOfOption[option];
+    require(opPos.exists, "Option does not exist");
+    votes[opPos.pos] = votes[opPos.pos] + 1;
     hasVoted[msg.sender] = true;
   }
 
