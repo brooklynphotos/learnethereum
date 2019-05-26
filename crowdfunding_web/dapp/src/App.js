00144c2b53ea3f48c7d966d687ce6984b848f89e
Brooklyn Photos <guoneng.list@gmail.com>
2019-05-26 23:13:58 +0200

    updated App.js and the ethereum javascripts

--- crowdfunding_web/dapp/src/App.js
+++ crowdfunding_web/dapp/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Router, Switch } from 'react-router-dom';
+import { Container, Menu } from 'semantic-ui-react';
+import history from './history';
+import { Campaign } from './components/Campaign';
+import { Home } from './components/Home';
+import { NotFound } from './components/NotFound';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router history={history}>
+    <Container>
+
+      <Menu secondary>
+        <Menu.Item
+          name='home'
+          onClick={this.navigateToHome}
+        />
+      </Menu>
+
+
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route path='/campaigns/:address' component={Campaign} />
+        <Route component={NotFound} />
+      </Switch>
+
+    </Container>
+  </Router>
+);
 }
 
 export default App;
